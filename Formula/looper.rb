7d3797dc8048f4d7ab52230a448a66d16f63b9e5
Kevin B. Ridgway <kevin@supernormal.com>
2026-04-17 17:43:21 -0400

    Update looper to v0.3.2

--- Formula/looper.rb
+++ Formula/looper.rb
@@ -1,8 +1,8 @@
 class Looper < Formula
   desc "CLI tool that plays a song on loop with a ratatui TUI and FFT visualizer"
   homepage "https://github.com/program247365/looper"
-  url "https://github.com/program247365/looper/archive/refs/tags/v0.3.1.tar.gz"
-  sha256 "4e9e16908d33666311c9d32b684dc078ea8314f2e4b66a36bd09e781abe657c5"
+  url "https://github.com/program247365/looper/archive/refs/tags/v0.3.2.tar.gz"
+  sha256 "e261ec25eaebe1026dc8bf9a1a5488f36da17c49d35ac6318f6e708d76772c22"
   license "MIT"
   head "https://github.com/program247365/looper.git", branch: "main"
 
