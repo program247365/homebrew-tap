792b55d761f92a43b64ca58fc21b1f60da3336c3
Kevin B. Ridgway <kevin@supernormal.com>
2026-07-08 11:36:15 -0400

    Update looper to v0.13.1

--- Formula/looper.rb
+++ Formula/looper.rb
@@ -1,13 +1,13 @@
 class Looper < Formula
   desc "CLI tool that plays a song on loop with a ratatui TUI and FFT visualizer"
   homepage "https://github.com/program247365/looper"
-  version "0.13.0"
+  version "0.13.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/program247365/looper/releases/download/v0.13.0/looper-aarch64-apple-darwin.tar.gz"
-      sha256 "f570d8e9601b7834323635b40fb31f8672998cd8f9c8b5973fa5945b9d189fdf"
+      url "https://github.com/program247365/looper/releases/download/v0.13.1/looper-aarch64-apple-darwin.tar.gz"
+      sha256 "f105c2323f94f81ee635c1488033a33a58e7750fd7a12a0082a70b2543c53a8f"
     end
   end
 
