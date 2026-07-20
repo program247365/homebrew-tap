4e0e245d375665dcc230561eaa36392cc8b6f66b
Kevin B. Ridgway <k@kbr.sh>
2026-07-20 15:19:22 -0400

    Update hackertuah to v0.4.0

--- Formula/hackertuah.rb
+++ Formula/hackertuah.rb
@@ -1,8 +1,8 @@
 class Hackertuah < Formula
   desc "Terminal UI for browsing Hacker News"
   homepage "https://github.com/program247365/hackertuah"
-  url "https://github.com/program247365/hackertuah/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  url "https://github.com/program247365/hackertuah/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "423a2d0c0df7386997057c4982ba721d3a8c15b360b2f75964cc24307a3d2c50"
   license "MIT"
   head "https://github.com/program247365/hackertuah.git", branch: "main"
 
